99812d3dfca158e0e6822bf9d20c27fe6424492e
Roy Andika <roy7andika@gmail.com>
2024-12-03 14:01:15 +0700

    Update rss-feed-parser.js

--- js/rss-feed-parser.js
+++ js/rss-feed-parser.js
@@ -11,7 +11,7 @@ async function fetchRSSFeed() {
     const feedContainer = document.getElementById("feed-container");
 
     // Membatasi hanya untuk 3 post terbaru
-    const posts = Array.from(items).slice(0, 3); // Ambil 3 item pertama
+    const posts = Array.from(items).slice(0, 12); // Ambil 3 item pertama
 
     posts.forEach(item => {
       const title = item.querySelector("title")?.textContent || "No Title";
@@ -26,14 +26,14 @@ async function fetchRSSFeed() {
       const postElement = document.createElement("div");
 
       postElement.innerHTML = `
-                    <div class="col-md-4">
-          <a href="${link}" target="_blank">${img ? `<img src="${img}" alt="${title}" class="responsive">` : ""}</a>
-          <div class="blog-text">
-            <span class="posted_on">${new Date(pubDate).toLocaleDateString()}</span>
-            <h4><a href="${link}" target="_blank">${title}</a></h4>
-            <p style="text-align:right;"><a href="${link}"><i class="icon-arrow-right22"></i></a></p>
-          </div>
-      </div>
+					<div class="col-md-3 text-center col-padding">
+						<a href="${link}" rel="nofollow" target="_blank" class="work" style="background-image: url(${img});" alt="${title}">
+							<div class="desc">
+								<h3>${title}</h3>
+								<span>${new Date(pubDate).toLocaleString()}</span>
+							</div>
+						</a>
+					</div>
                 `;
 
       feedContainer.appendChild(postElement);
